83f5c2230b1c18de6d8ff612e8c8f66fd142d11b
vincent <thevinhnguyen020302@gmail.com>
2026-09-07 14:45:28 +0300

    set up Fund 1

---
+++ Fundamental_1/script.js
@@ -0,0 +1,19 @@
+// let user = "Vinh"
+
+// if(user === "Vinh") {
+//     alert("So Handsome !!!")
+//     console.log(typeof user)
+// }
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
