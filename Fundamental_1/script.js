a8a71813bba5464cb32e20d604a7e744131efeb1
vincent <thevinhnguyen020302@gmail.com>
2026-09-08 21:05:38 +0300

    done switch

--- Fundamental_1/script.js
+++ Fundamental_1/script.js
@@ -1,19 +1,59 @@
-// let user = "Vinh"
+// // let user = "Vinh"
 
-// if(user === "Vinh") {
-//     alert("So Handsome !!!")
-//     console.log(typeof user)
+// // if(user === "Vinh") {
+// //     alert("So Handsome !!!")
+// //     console.log(typeof user)
+// // }
+// // let, const and var
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+// // const job;
+
+// var job = 'programmer';
+// job = 'teacher'
+
+// lastName = 'Schmedtmann';
+
+// // let n = '1' + 1; // '11'
+// // // n = n - 1;
+// // console.log(typeof n,n);
+
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10');
+// console.log('23' / '2');
+
+// const money = 0
+
+// if (money) {
+//     console.log("Dont spend too much")
+// } else {
+//     console.log("Should get a job")
 // }
-// let, const and var
-let age = 30;
-age = 31;
 
-const birthYear = 1991;
-// birthYear = 1990;
-// const job;
+const day = 'friday';
 
-var job = 'programmer';
-job = 'teacher'
-
-lastName = 'Schmedtmann';
-console.log(lastName);
+switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
