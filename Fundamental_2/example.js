a5871006fb20058e586493e5067e064561645268
vincent <thevinhnguyen020302@gmail.com>
2026-09-09 13:30:47 +0300

    done declaration and expression func

--- Fundamental_2/example.js
+++ Fundamental_2/example.js
@@ -7,6 +7,26 @@ function final_value(bill) {
     return total
 }
 
-console.log(final_value(275))
+// console.log(final_value(275))
 
 
+//function declaration
+
+// const age1 = calcAge1(2002) => this line is the difference between declaration and expression , declare could be called before defined
+
+function calcAge1(birthYear) {
+    return 2026 - birthYear
+}
+const age1 = calcAge1(2002)
+
+
+//function expression , function without name is called anonymous function => need to store inside variable
+const calcAge2 = function (birtYeah) {
+    return 2027-birtYeah
+}
+
+const age2 = calcAge2(2000)
+
+console.log(age1,age2)
+
+//Arrow Function 
